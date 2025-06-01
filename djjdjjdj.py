2e79c71aa3248a06273cadeb0fdb5a217d75acf0
BuildTools <unconfigured@null.spigotmc.org>
2025-06-01 14:02:30 +0300

    first commit

---
+++ djjdjjdj.py
@@ -0,0 +1,18 @@
+iloveyou = 0
+for x in range(1,99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
+    iloveyou = iloveyou + 1
+    print("I love you", iloveyou, "%❤️")
+    iloveyou = iloveyou + 1
+    print(" I love you", iloveyou, "%🧡")
+    iloveyou = iloveyou + 1
+    print("  I love you", iloveyou, "%💛")
+    iloveyou = iloveyou + 1
+    print("   I love you", iloveyou, "%💚")
+    iloveyou = iloveyou + 1
+    print("    I love you", iloveyou, "%💙")
+    iloveyou = iloveyou + 1
+    print("   I love you", iloveyou, "%💜")
+    iloveyou = iloveyou + 1
+    print("  I love you", iloveyou, "%🤍")
+    iloveyou = iloveyou + 1
+    print(" I love you", iloveyou, "%🖤")
